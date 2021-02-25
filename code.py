dcf37ea1d93b1d7a48d3af99dd77234101d3471c
99003765 <78848640+99003765@users.noreply.github.com>
2021-02-25 12:38:32 +0530

    comments updated

--- code.py
+++ code.py
@@ -10,47 +10,36 @@ Date  : 22/02/2021
 
 '''
 
-import re             # importing Regular Expression Package
-
-
-class Find_Word:                   # Creating Class
-
-    def __init__(self, pattern, f):        # initialising variables
+import re  # importing Regular Expression Package
+class Find_Word:  # creating class
+    def __init__(self, pattern, f):  # initialising variables
         self.pattern = pattern
         self.f = f
-
-
-    def pattern_search(self,):             # Creating methods for pattern search
-
-        count = 0                           # Initialising search count to zero
-        
-        match = re.findall(self.pattern, self.f, re.M | re.I)          # Searching given pattern in the input file and ignoring alphabatic cases
+    def pattern_search(self,):  # Creating methods for pattern search
+        count = 0  # Initialising search count to zero
+        match = re.findall(self.pattern, self.f, re.M | re.I)
+        # Searching for given pattern and ignoring cases
         extension = ".txt"
-        file_name = str(self.pattern) + extension                     # Converting pattern given to string and adding txt extension
-        f_out = open(file_name, "w+")                                 # Creating and opening output file with Read and write mode
-        f_out.write("The count of words:" + str(len(match)))          # Displaying word count         
+        file_name = str(self.pattern) + extension
+        # Converting pattern given to string and adding txt extension
+        f_out = open(file_name, "w+")  # opening o/p file with Read,write mode
+        f_out.write("The count of words:" + str(len(match)))
         f_out.write(str("\n\n"))
-        split_f = self.f.split()                                      # Splitting up of sentences to words of input file
+        split_f = self.f.split()  # Splitting up of sentences
         f = open(file_name, "a+")
-        for i in range(len(split_f)):                                 # opening input file in apend, write,read mdoe
-            match = re.match(self.pattern, split_f[i], re.M | re.I)   # matching the pattern to splitted words
+        for i in range(len(split_f)):   # opening input file in apend mode
+            match = re.match(self.pattern, split_f[i], re.M | re.I)
+            # matching pattern to splitted words
             if match:
-                count += 1                                            
-                String = (split_f[i-1]+" "+split_f[i]+" "+split_f[i+1]+"\n")      # creating the matched string including its previous and next words
+                count += 1                        
+                String = (split_f[i-1]+" "+split_f[i]+" "+split_f[i+1]+"\n")
+                # creating the matched string
                 f.writelines(str(String))
 
-
-
-
-if __name__ == '__main__':                                                      # creating main 
-
-
-    with open('input.txt') as input_file:                                       # opening input.txt
-        read_file = input_file.read()                                           # reading innput file
-        
-    for x in range(5):                                                          # Searching for maximum 5 words at a stretch
+if __name__ == '__main__':  # creating main
+    with open('input.txt') as input_file:  # opening input.txt
+        read_file = input_file.read()  # reading innput file
+    for x in range(5):  # Searching for maximum 5 words at a stretch
         keyword = input("Enter the keyword you wanted to search in file:")
-        d = Find_Word(keyword, read_file)                                       # creation of objects and pointing to class
-        d.pattern_search()
-
-
+d = Find_Word(keyword, read_file)   # creation of objects and pointing to class
+d.pattern_search()      

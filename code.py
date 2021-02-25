5a888a067cf908db167043f0f6a33eb51a969e75
99003765 <78848640+99003765@users.noreply.github.com>
2021-02-25 08:46:09 +0530

    updated

--- code.py
+++ code.py
@@ -1,16 +1,16 @@
 import re
 
 
-class Find_Word():
+class Find_Word:
 
     def __init__(self, pattern, f):
-        self.pattern = pattern()
+        self.pattern = pattern
         self.f = f
 
     def pattern_search(self,):
 
         count = 0
-        match = re.findall(self.pattern(), self.f, re.M | re.I)
+        match = re.findall(self.pattern, self.f, re.M | re.I)
         extension = ".txt"
         file_name = str(self.pattern) + extension
         f_out = open(file_name, "w+")
@@ -18,11 +18,23 @@ class Find_Word():
         f_out.write(str("\n\n"))
         split_f = self.f.split()
         f = open(file_name, "a+")
-        for i in range(len(split_file_text)):
-            match = re.match(self.pattern, split_file_text[i], re.M | re.I)
+        for i in range(len(split_f)):
+            match = re.match(self.pattern, split_f[i], re.M | re.I)
             if match:
                 count += 1
-                x = (split_f[i-1]+" "+split_f[i]+" "+split_f[i+1]+"\n")
-                f.writelines(str(x))
+                String = (split_f[i-1]+" "+split_f[i]+" "+split_f[i+1]+"\n")
+                f.writelines(str(String))
+
+
+
+
+if __name__ == '__main__':
+
+    with open('input.txt') as input_file:
+        read_file = input_file.read()
+    for x in range(5):
+        keyword = input("Enter the keyword you wanted to search in file:")
+        d = Find_Word(keyword, read_file)
+        d.pattern_search()
 
 

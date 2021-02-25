5ec0a2465d641795bcb045a481d3e0cf10382bfd
99003765 <78848640+99003765@users.noreply.github.com>
2021-02-25 11:58:57 +0530

    comments included

--- code.py
+++ code.py
@@ -10,45 +10,47 @@ Date  : 22/02/2021
 
 '''
 
-import re
-class Find_Word:
+import re             # importing Regular Expression Package
 
-    def __init__(self, pattern, f):
+
+class Find_Word:                   # Creating Class
+
+    def __init__(self, pattern, f):        # initialising variables
         self.pattern = pattern
         self.f = f
 
 
-    def pattern_search(self,):
+    def pattern_search(self,):             # Creating methods for pattern search
 
-        count = 0
+        count = 0                           # Initialising search count to zero
         
-        match = re.findall(self.pattern, self.f, re.M | re.I)
+        match = re.findall(self.pattern, self.f, re.M | re.I)          # Searching given pattern in the input file and ignoring alphabatic cases
         extension = ".txt"
-        file_name = str(self.pattern) + extension
-        f_out = open(file_name, "w+")
-        f_out.write("The number of occurrences of keyword is:" + str(len(match)))
+        file_name = str(self.pattern) + extension                     # Converting pattern given to string and adding txt extension
+        f_out = open(file_name, "w+")                                 # Creating and opening output file with Read and write mode
+        f_out.write("The count of words:" + str(len(match)))          # Displaying word count         
         f_out.write(str("\n\n"))
-        split_f = self.f.split()
+        split_f = self.f.split()                                      # Splitting up of sentences to words of input file
         f = open(file_name, "a+")
-        for i in range(len(split_f)):
-            match = re.match(self.pattern, split_f[i], re.M | re.I)
+        for i in range(len(split_f)):                                 # opening input file in apend, write,read mdoe
+            match = re.match(self.pattern, split_f[i], re.M | re.I)   # matching the pattern to splitted words
             if match:
-                count += 1
-                String = (split_f[i-1]+" "+split_f[i]+" "+split_f[i+1]+"\n")
+                count += 1                                            
+                String = (split_f[i-1]+" "+split_f[i]+" "+split_f[i+1]+"\n")      # creating the matched string including its previous and next words
                 f.writelines(str(String))
 
 
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':                                                      # creating main 
 
 
-    with open('input.txt') as input_file:
-        read_file = input_file.read()
+    with open('input.txt') as input_file:                                       # opening input.txt
+        read_file = input_file.read()                                           # reading innput file
         
-    for x in range(5):
+    for x in range(5):                                                          # Searching for maximum 5 words at a stretch
         keyword = input("Enter the keyword you wanted to search in file:")
-        d = Find_Word(keyword, read_file)
+        d = Find_Word(keyword, read_file)                                       # creation of objects and pointing to class
         d.pattern_search()
 
 
